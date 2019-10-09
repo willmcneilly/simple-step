e35baf2f09aa60d3c4b183d25ed17f558cff2634
Will McNeilly <willmcneilly@me.com>
2019-10-09 12:39:30 +0100

    update sequences from reducer

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import { Howler, Howl } from 'howler';
 import { IoIosPlay, IoIosSquare } from 'react-icons/io';
 import './App.css';
 
-const data = {
-  timeSignature: '4/4',
-  tempo: 120,
-  length: 16,
-  stepLength: '1/8 NOTE',
-  sequence: [
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1]
-  ],
-  instrumentSlots: { 0: 'KICK', 1: 'SNARE' }
-};
-
 const kick = new Howl({
   src: ['kick.wav']
 });
@@ -62,6 +50,27 @@ function stepReducer(state, action) {
       return { ...state, step: state.step === 16 ? 1 : state.step + 1 };
     case 'setBPM':
       return { ...state, bpm: action.bpm };
+    case 'updateSequenceAtIndex':
+      return {
+        ...state,
+        sequenceArrayData: {
+          ...state.sequenceArrayData,
+          [action.sequenceId]: {
+            ...state.sequenceArrayData[action.sequenceId],
+            sequence: Object.assign(
+              [],
+              state.sequenceArrayData[action.sequenceId].sequence,
+              {
+                [action.updateAtIndex]: state.sequenceArrayData[
+                  action.sequenceId
+                ]['sequence'][action.updateAtIndex]
+                  ? 0
+                  : 1
+              }
+            )
+          }
+        }
+      };
     default:
       throw new Error(`${action.type} unknown`);
   }
@@ -73,15 +82,49 @@ function App() {
   const [state, dispatch] = useReducer(stepReducer, {
     step: 1,
     bpm: 120,
-    isPlaying: false
+    isPlaying: false,
+    sequenceArrayOrder: [
+      'kick',
+      'snare',
+      'snareRim',
+      'hat',
+      'crash',
+      'floorTom'
+    ],
+    sequenceArrayData: {
+      kick: {
+        id: 'kick',
+        label: 'Kick',
+        sequence: initialSequence,
+        sample: kick
+      },
+      snare: {
+        id: 'snare',
+        label: 'Snare',
+        sequence: initialSequence,
+        sample: snare
+      },
+      snareRim: {
+        id: 'snareRim',
+        label: 'Snare Rim',
+        sequence: initialSequence,
+        sample: snareRim
+      },
+      hat: { id: 'hat', label: 'Hat', sequence: initialSequence, sample: hat },
+      crash: {
+        id: 'crash',
+        label: 'Crash',
+        sequence: initialSequence,
+        sample: crash
+      },
+      floorTom: {
+        id: 'floorTom',
+        label: 'Floor Tom',
+        sequence: initialSequence,
+        sample: floorTom
+      }
+    }
   });
-  const [kickSequence, setKickSequence] = useState(initialSequence);
-  const [snareSequence, setSnareSequence] = useState(initialSequence);
-  const [snareRimSequence, setSnareRimSequence] = useState(initialSequence);
-  const [hatSequence, setHatSequence] = useState(initialSequence);
-  const [crashSequence, setCrashSequence] = useState(initialSequence);
-  const [floorTomSequence, setFloorTomSequence] = useState(initialSequence);
-
   const beatLength = ((60 / state.bpm) * 1000) / 4;
 
   useAnimationFrame(
@@ -96,101 +139,37 @@ function App() {
     if (!state.isPlaying) {
       return;
     }
-    if (kickSequence[state.step - 1] === 1) {
-      kick.play();
-    }
-    if (snareSequence[state.step - 1] === 1) {
-      snare.play();
-    }
-    if (snareRimSequence[state.step - 1] === 1) {
-      snareRim.play();
-    }
-    if (hatSequence[state.step - 1] === 1) {
-      hat.play();
-    }
-
-    if (crashSequence[state.step - 1] === 1) {
-      crash.play();
-    }
-
-    if (floorTomSequence[state.step - 1] === 1) {
-      floorTom.play();
-    }
+    state.sequenceArrayOrder.forEach(sequenceId => {
+      const data = state.sequenceArrayData[sequenceId];
+      if (data.sequence[state.step - 1] === 1) {
+        data.sample.play();
+      }
+    });
   }, [state.step, state.isPlaying]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <button
-          style={{
-            backgroundColor: 'transparent',
-            color: '#eee',
-            border: 0,
-            padding: '20px 20px 10px 20px',
-            fontSize: '60px',
-            margin: 0,
-            marginBottom: '40px'
-          }}
-          onClick={() => {
-            state.isPlaying
-              ? dispatch({ type: 'pause' })
-              : dispatch({ type: 'play' });
-          }}
-        >
-          {state.isPlaying ? <IoIosSquare /> : <IoIosPlay />}
-        </button>
-        <SequenceArray
-          label="Kick"
-          sequence={kickSequence}
-          setSequence={setKickSequence}
-          step={state.step}
-        />
-        <SequenceArray
-          label="Snare"
-          sequence={snareSequence}
-          setSequence={setSnareSequence}
-          step={state.step}
-        />
-        <SequenceArray
-          label="Snare Rim"
-          sequence={snareRimSequence}
-          setSequence={setSnareRimSequence}
-          step={state.step}
-        />
-        <SequenceArray
-          label="Closed Hat"
-          sequence={hatSequence}
-          setSequence={setHatSequence}
-          step={state.step}
-        />
-        <SequenceArray
-          label="Crash"
-          sequence={crashSequence}
-          setSequence={setCrashSequence}
-          step={state.step}
-        />
-        <SequenceArray
-          label="Floor Tom"
-          sequence={floorTomSequence}
-          setSequence={setFloorTomSequence}
-          step={state.step}
-        />
-        <label
-          style={{ fontSize: '12px', fontWeight: 'bold', marginTop: '40px' }}
-        >
-          BPM
-          <input
-            onChange={e => dispatch({ type: 'setBPM', bpm: e.target.value })}
-            type="number"
-            value={state.bpm}
-            style={{
-              marginLeft: '5px',
-              width: '50px',
-              fontSize: '14px',
-              fontWeight: 'bold'
-            }}
-          />
-        </label>
+        <PlayButton isPlaying={state.isPlaying} dispatch={dispatch} />
+        {state.sequenceArrayOrder.map(sequenceId => {
+          const sequenceData = state.sequenceArrayData[sequenceId];
+          return (
+            <SequenceArray
+              key={sequenceData.id}
+              label={sequenceData.label}
+              step={state.step}
+              sequence={sequenceData.sequence}
+              setSequence={idx =>
+                dispatch({
+                  type: 'updateSequenceAtIndex',
+                  sequenceId: sequenceData.id,
+                  updateAtIndex: idx
+                })
+              }
+            />
+          );
+        })}
+        <BPMAdjustment bpm={state.bpm} dispatch={dispatch} />
       </header>
     </div>
   );
@@ -222,15 +201,11 @@ function SequenceArray({ sequence, step, setSequence, grouping = 4, label }) {
         {sequence.map((val, idx) => (
           <button
             onMouseDown={_ => {
-              setSequence(seq =>
-                Object.assign([], seq, { [idx]: seq[idx] ? 0 : 1 })
-              );
+              setSequence(idx);
             }}
             onMouseEnter={_ => {
               if (mouseDown) {
-                setSequence(seq =>
-                  Object.assign([], seq, { [idx]: seq[idx] ? 0 : 1 })
-                );
+                setSequence(idx);
               }
             }}
             key={`kick-${idx}`}
@@ -249,6 +224,46 @@ function SequenceArray({ sequence, step, setSequence, grouping = 4, label }) {
         ))}
       </div>
     </div>
+  );
+}
+
+function PlayButton({ isPlaying, dispatch }) {
+  return (
+    <button
+      style={{
+        backgroundColor: 'transparent',
+        color: '#eee',
+        border: 0,
+        padding: '20px 20px 10px 20px',
+        fontSize: '60px',
+        margin: 0,
+        marginBottom: '40px'
+      }}
+      onClick={() => {
+        isPlaying ? dispatch({ type: 'pause' }) : dispatch({ type: 'play' });
+      }}
+    >
+      {isPlaying ? <IoIosSquare /> : <IoIosPlay />}
+    </button>
+  );
+}
+
+function BPMAdjustment({ dispatch, bpm }) {
+  return (
+    <label style={{ fontSize: '12px', fontWeight: 'bold', marginTop: '40px' }}>
+      BPM
+      <input
+        onChange={e => dispatch({ type: 'setBPM', bpm: e.target.value })}
+        type="number"
+        value={bpm}
+        style={{
+          marginLeft: '5px',
+          width: '50px',
+          fontSize: '14px',
+          fontWeight: 'bold'
+        }}
+      />
+    </label>
   );
 }
 
